31f930bab9890652b5a28ac6eb17e5ae851a53fd
raagul001 <2k20ece057@kiot.ac.in>
2025-03-06 18:27:47 +0530

    Sample commit

--- amazon/urls.py
+++ amazon/urls.py
@@ -15,5 +15,5 @@ urlpatterns = [
     path('collections/<str:name>',views.details,name='collections'),
     path('collections/<str:cname>/<str:pname>',views.product_details,name='product_details'),
     path('addtocart',views.add_to_cart,name='addtocart'),
-
+    
 ]
